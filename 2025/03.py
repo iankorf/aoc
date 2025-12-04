1f85826c9210544f3901c904ea4ee8bd0aea4618
Ian Korf <ifkorf@gmail.com>
2025-12-03 20:55:08 -0800

    up

--- 2025/03.py
+++ 2025/03.py
@@ -1,9 +1,18 @@
-import json
 import sys
-
-
 
 total = 0
 with open(sys.argv[1]) as fp:
 	for line in fp:
 		seq = line.rstrip()
+		i = 0
+		built = ''
+		while len(built) < 12:		
+			mln = '0'
+			for j in range(i, len(seq)):
+				c = seq[j]
+				if c > mln and j < len(seq) - 12 + len(built) +1:
+					mln = c			
+			built += mln
+			i = seq.index(mln, i) + 1
+		total += int(built)
+print(total)
