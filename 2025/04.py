a3a3ec81ff0c568919beb40fb046387725ebf4f6
Ian Korf <ifkorf@gmail.com>
2025-12-03 21:49:41 -0800

    up

--- 2025/04.py
+++ 2025/04.py
@@ -1,11 +1,17 @@
 import sys
 
+dirs = [
+	(-1,-1), (-1,0), (-1,+1),
+	( 0,-1),         ( 0,+1),
+	(+1,-1), (+1,0), (+1,+1),
+]
+
 with open(sys.argv[1]) as fp:
 	grid = []
 	for line in fp:
 		row = list(line.rstrip())
 		grid.append(row)
-	
+
 removed = 0
 last = -1
 while removed != last:
@@ -14,16 +20,14 @@ while removed != last:
 		for x in range(len(grid[y])):
 			if grid[y][x] != '@': continue
 			n = 0 # neighbors
-			for dy in (-1, 0, 1):
-				for dx in (-1, 0, 1):
-					if dy == 0 and dx == 0: continue
-					sy = y + dy
-					sx = x + dx
-					if sy < 0: continue
-					if sx < 0: continue
-					if sy >= len(grid): continue
-					if sx >= len(grid[y]): continue
-					if grid[sy][sx] == '@': n += 1
+			for dy, dx in dirs:
+				sy = y + dy
+				sx = x + dx
+				if sy < 0: continue
+				if sx < 0: continue
+				if sy >= len(grid): continue
+				if sx >= len(grid[y]): continue
+				if grid[sy][sx] == '@': n += 1
 			if n <= 3: found.append( (y, x) )
 
 	last = removed
